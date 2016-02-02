bf152e3205ec7397b67704e67e377405050ef882
Bryan Dixon <javawolfpack@gmail.com>
2016-02-02 15:15:45 -0800

    Lecture 3 Simple UI
    Signed-off-by: Bryan Dixon <javawolfpack@gmail.com>

---
+++ SimpleUILecture3/app/src/main/java/examples/csci567/simpleuilecture3/MainActivity.java
@@ -0,0 +1,27 @@
+package examples.csci567.simpleuilecture3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textView1);
+        textView.setText("Text from the android code");
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Button Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
